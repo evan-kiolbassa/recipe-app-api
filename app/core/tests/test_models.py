1b2bf24c4bd52ebbd5dfb39bbaae080821cb76e1
evan-kiolbassa <evankiolbassa@hotmail.com>
2022-06-19 09:46:45 -0500

    Added email  normalization functionality and test cases for CI

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -32,7 +32,7 @@ class ModelTests(TestCase):
         ['test1@EXAMPLE.com', 'test1@example.com'],
         ['Test2@Example.com', 'Test2@example.com'],
         ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
-        ['test4@example.COM', 'test4@example.com'],
+        ['test4@example.COM', 'test4@example.com']
         ]
         for email, expected in smaple_emails:
             user = get_user_model().objects.create_user(email, 'sample 123')
