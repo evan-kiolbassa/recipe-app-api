e53afea2b3dbcb97a86c2500db52d205ad3715dd
evan-kiolbassa <evankiolbassa@hotmail.com>
2022-06-26 21:31:52 -0500

    Configured django admin user customization features

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin  # Noqa
+'''
+Django admin customization
+'''
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    '''
+    Define the admin pages for users.
+    '''
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,39 @@
+'''
+Tests for the Django admin modification
+'''
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    '''
+    Tests for django admin
+    '''
+
+    def setUp(self):
+        '''
+        Create users and client to mock tests
+        '''
+        self.client = Client() # Creating a client for test cases
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpass123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
+        )
+
+    def test_users_list(self):
+        '''
+        Tests that users appear on Django Admin User-Interface
+        '''
+        url = reverse('admin:core_user_changelist') # Accesses list of users in sytsem
+        res = self.client.get(url) # Makes a http get request
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
