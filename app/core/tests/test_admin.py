59a4d3cb82302127f2799ba4890267c47d92d834
evan-kiolbassa <evankiolbassa@hotmail.com>
2022-07-10 12:17:44 -0500

    Added functionality to administrator UI to create users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -50,3 +50,11 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
         # Assures page loads properly with a 200 HTTP respone
         self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        '''
+        Test the create user page works
+        '''
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
